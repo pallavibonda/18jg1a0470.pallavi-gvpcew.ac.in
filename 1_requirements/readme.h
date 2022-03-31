57aab4e1aa15513f471b005a74db26ae1ade5a90
Pallavi bonda <101260066+pallavibonda@users.noreply.github.com>
2022-03-31 18:35:50 +0530

    readme.h

---
+++ 1_requirements/readme.h
@@ -0,0 +1,19 @@
+REQUIREMENTS
+Problem
+How to cast vote, count, and display the votes for each candidate that participates in elections using C language?
+  
+  Solution
+Let’s consider three persons who participated in elections. Here we need to write a code for the following −
+
+Cast vote − Selecting a candidate by pressing the cast vote
+
+Find vote count − Finding the total number of votes each candidate gains declaring the winner.
+  
+  Pros
+Reusable code that can be adapted into other systems.
+Reprogrammable according to the particular programmers taste.
+Better and more efficient in comparison with traditional voting system.
+
+  Cons
+No GUI (Graphical User Interface).
+Can be more secure.
