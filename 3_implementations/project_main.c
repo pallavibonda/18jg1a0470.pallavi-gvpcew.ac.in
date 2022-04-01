9993fd95387bc52efaf329225066f20043d44ebf
Pallavi bonda <101260066+pallavibonda@users.noreply.github.com>
2022-04-01 06:52:19 +0530

    Update project_main.c

--- 3_implementations/project_main.c
+++ 3_implementations/project_main.c
@@ -2,10 +2,10 @@
 
 #define CANDIDATE_COUNT
 
-#define CANDIDATE1 "David Hull"
-#define CANDIDATE2 "Kristin Canella"
-#define CANDIDATE3 "Jim Brar"
-#define CANDIDATE4 "Donald Truimph"
+#define CANDIDATE1 "PAWAN KALYAN"
+#define CANDIDATE2 "YS JAGAN MOHAN REDDY"
+#define CANDIDATE3 "CHANDRA BABU NAIDU"
+#define CANDIDATE4 "KCR"
 
 int votesCount1=0, votesCount2=0, votesCount3=0, votesCount4=0, spoiledtvotes=0;
 
